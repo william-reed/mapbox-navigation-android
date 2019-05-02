5109f2bd5afaeb6bfb9f8a2df3febd18bfa96c02
Pablo Guardiola <pablo.guardiola@mapbox.com>
2019-05-02 13:21:27 -0400

    [WIP] move navigation service (and dependent classes) into the internal package (#1924)

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationConstants.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationConstants.java
@@ -3,6 +3,8 @@ package com.mapbox.services.android.navigation.v5.navigation;
 import android.support.annotation.IntDef;
 import android.support.annotation.StringDef;
 
+import com.mapbox.services.android.navigation.v5.internal.navigation.RouteProcessorBackgroundThread;
+
 /**
  * Navigation constants
  *
